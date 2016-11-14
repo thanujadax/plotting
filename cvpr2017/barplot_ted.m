bc45782e6bdef904fe155fbf9a21f4a5a0a9aa44
Thanuja Ambegoda <thanujadax@gmail.com>
2016-11-14 12:31:30 +0100

    init

---
+++ cvpr2017/barplot_ted.m
@@ -0,0 +1,71 @@
+function barplot_ted()
+
+% TED errors to plot : gc,gala,stc
+% RFC
+fp_rfc = [0.160298, 0 ,0.013048];
+fn_rfc = [0.013979, 0.073625, 0.158434];
+fs_rfc = [0.191985, 0.001864, 0.050326];
+fm_rfc = [0.649581, 0.712954, 0.143523];
+tot_rfc = [1.015843, 0.788444, 0.365331];
+
+fp_cnn = [0.039143, 0.00932, 0.016775];
+fn_cnn = [0.015843, 0.212488, 0.089469];
+fs_cnn = [0.032619, 0.007456, 0.011184];
+fm_cnn = [0.245107, 0.205965, 0.108108];
+tot_cnn = [0.332712, 0.435228, 0.225536];
+
+
+% y = [-log(fp);
+%     -log(fn);
+%     -log(fs);
+%     -log(fm)];
+%% FM and FS for RFC
+y = [fm_rfc; fs_rfc];
+figure(1);
+bar(y)
+legend('GC','Gala','STC')
+set(gca,'XTickLabel',{'FM' 'FS'})
+
+x0=10;
+y0=10;
+width=180;
+height=150;
+set(gcf,'units','points','position',[x0,y0,width,height])
+
+%% FM and FS for RFC
+y = [fm_cnn; fs_cnn];
+figure(2);
+bar(y)
+legend('GC','Gala','STC')
+set(gca,'XTickLabel',{'FM' 'FS'})
+
+x0=10;
+y0=10;
+width=180;
+height=150;
+set(gcf,'units','points','position',[x0,y0,width,height])
+%% FP and FN for RFC
+y = [fp_rfc; fn_rfc];
+figure(3);
+bar(y)
+% legend('GC','Gala','STC')
+set(gca,'XTickLabel',{'FP' 'FN'})
+
+x0=10;
+y0=10;
+width=180;
+height=150;
+set(gcf,'units','points','position',[x0,y0,width,height])
+
+%% FM and FS for RFC
+y = [fp_cnn; fn_cnn];
+figure(4);
+bar(y)
+% legend('GC','Gala','STC')
+set(gca,'XTickLabel',{'FP' 'FN'})
+
+x0=10;
+y0=10;
+width=180;
+height=150;
+set(gcf,'units','points','position',[x0,y0,width,height])
